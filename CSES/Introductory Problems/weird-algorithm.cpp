a7c7bafd8b517375271b21e3d4551b68482f2e4e
pathmarkoak <omkarp32@gmail.com>
2023-07-30 18:01:46 -0400

    added .vscode configs

--- CSES/Introductory Problems/weird-algorithm.cpp
+++ CSES/Introductory Problems/weird-algorithm.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main() {
-    
+    long long n;
+    cin >> n;
+    while (n != 1) {
+        cout << n << " ";
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    }
+    cout << 1;
 }
