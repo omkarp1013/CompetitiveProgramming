ec7b0b1366d35f26cce5da0e58107d00f9534325
pathmarkoak <omkarp32@gmail.com>
2023-08-02 23:06:19 -0400

    started Two Sets

---
+++ CSES/two-sets.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; 
+    cin >> n;
+    if (((n * (n+1))/2) % 2 == 0) {
+        cout << "YES" << endl;
+        
+    } else {
+        cout << "NO";
+        return 0;
+    }
+}
