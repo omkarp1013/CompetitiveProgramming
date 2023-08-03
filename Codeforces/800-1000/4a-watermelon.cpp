e2d4c39a32ba620f49e5b139dee0fb694b5cdd85
pathmarkoak <omkarp32@gmail.com>
2023-08-02 23:14:47 -0400

    4A Watermelon

---
+++ Codeforces/800-1000/4a-watermelon.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n > 3 && n % 2 == 0) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+}
